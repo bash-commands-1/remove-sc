367af1ad808c3f5bfed85dfd20bc4fecda18b0e0
bash-commands-1 <54801045+bash-commands-1@users.noreply.github.com>
2022-07-25 09:44:04 +1000

    Add files via upload

---
+++ Remove-SC.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd /opt
+SCID=$(ls | grep "connect" | sed 's/\.app//g' | cut -d'-' -f2)
+
+launchctl unload /Library/LaunchDaemons/connectwisecontrol-$SCID.plist
+launchctl unload /Library/LaunchAgents/connectwisecontrol-$SCID-onlogin.plist
+launchctl unload /Library/LaunchAgents/connectwisecontrol-$SCID-prelogin.plist
+
+pkill "CT Sapphire Client"
+
+rm -rf /opt/connectwisecontrol-$SCID.app
+rm -rf /Library/LaunchDaemons/connectwisecontrol-$SCID.plist
+rm -rf /Library/LaunchAgents/connectwisecontrol-$SCID-onlogin.plist
+rm -rf /Library/LaunchAgents/connectwisecontrol-$SCID-prelogin.plist
