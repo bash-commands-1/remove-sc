fb106baff09d9af6b582e6fe19373d0a545b3e6d
bash-commands-1 <54801045+bash-commands-1@users.noreply.github.com>
2022-07-25 10:19:36 +1000

    Update Remove-SC.sh

--- Remove-SC.sh
+++ Remove-SC.sh
@@ -3,15 +3,16 @@
 cd /opt
 SCID=$(ls | grep "connect" | sed 's/\.app//g' | cut -d'-' -f2)
 
-#launchctl unload /Library/LaunchDaemons/connectwisecontrol-$SCID.plist
-#launchctl unload /Library/LaunchAgents/connectwisecontrol-$SCID-onlogin.plist
-#launchctl unload /Library/LaunchAgents/connectwisecontrol-$SCID-prelogin.plist
+launchctl unload /Library/LaunchDaemons/connectwisecontrol-$SCID.plist
+launchctl unload /Library/LaunchAgents/connectwisecontrol-$SCID-onlogin.plist
+launchctl unload /Library/LaunchAgents/connectwisecontrol-$SCID-prelogin.plist
 
-#pkill "CT Sapphire Client"
+pkill "CT Sapphire Client"
 
-#rm -rf /opt/connectwisecontrol-$SCID.app
-#rm -rf /Library/LaunchDaemons/connectwisecontrol-$SCID.plist
-#rm -rf /Library/LaunchAgents/connectwisecontrol-$SCID-onlogin.plist
-#rm -rf /Library/LaunchAgents/connectwisecontrol-$SCID-prelogin.plist
+rm -rf /opt/connectwisecontrol-$SCID.app
+rm -rf /Library/LaunchDaemons/connectwisecontrol-$SCID.plist
+rm -rf /Library/LaunchAgents/connectwisecontrol-$SCID-onlogin.plist
+rm -rf /Library/LaunchAgents/connectwisecontrol-$SCID-prelogin.plist
+
 sleep 10
 rm -- "$0"
