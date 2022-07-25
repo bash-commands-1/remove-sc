82792113519e4aeabd942c5e3f440bc79b2006dd
bash-commands-1 <54801045+bash-commands-1@users.noreply.github.com>
2022-07-25 10:23:53 +1000

    Update Remove-SC.sh

--- Remove-SC.sh
+++ Remove-SC.sh
@@ -3,16 +3,20 @@
 cd /opt
 SCID=$(ls | grep "connect" | sed 's/\.app//g' | cut -d'-' -f2)
 
+#Unload screenconnect
 launchctl unload /Library/LaunchDaemons/connectwisecontrol-$SCID.plist
 launchctl unload /Library/LaunchAgents/connectwisecontrol-$SCID-onlogin.plist
 launchctl unload /Library/LaunchAgents/connectwisecontrol-$SCID-prelogin.plist
 
+#Kill client
 pkill "CT Sapphire Client"
 
+#Remove app and launchD and launchAgents
 rm -rf /opt/connectwisecontrol-$SCID.app
 rm -rf /Library/LaunchDaemons/connectwisecontrol-$SCID.plist
 rm -rf /Library/LaunchAgents/connectwisecontrol-$SCID-onlogin.plist
 rm -rf /Library/LaunchAgents/connectwisecontrol-$SCID-prelogin.plist
 
+#Remove script
 sleep 10
 rm -- "$0"
